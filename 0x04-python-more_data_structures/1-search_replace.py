8b75f88df07de09f6e22b0786f3d8b2e8fec7cba
Awokay56 <kgbenga56@gmail.com>
2023-12-10 08:13:37 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-#1-search_replace.py
+# 1-search_replace.py
 
 def search_replace(my_list, search, replace):
     if my_list is None:
         return
-    new_list=my_list[:]
+    new_list  = my_list[:]
     for idx, d in enumerate(new_list):
         if d == search:
             new_list[idx] = replace
