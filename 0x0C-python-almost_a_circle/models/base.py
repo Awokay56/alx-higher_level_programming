dd8a84da5d65a6e3335739c3f451bcacda4b351b
kayode <kgbenga56@gmail.com>
2024-01-15 03:48:34 -0800

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+defines a base model class
+"""
+
+class Base:
+    """
+    Represents the base model
+    """
+
+    __nb__objects = 0
+    def ___init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base__nb__objects += 1
+            self.id = Base__nb__objects
+
+            if __name__=="__main__":
+                b1 = Base()
+                print(b1.id)
+
+                b2 = Base()
+                print(b2.id)
+
+                b3 = Base()
+                print(b3.id)
+
+                b4 = Base(12)
+                print(b4.id)
+
+                b5 = Base()
+                print(b5.id)
+
+
