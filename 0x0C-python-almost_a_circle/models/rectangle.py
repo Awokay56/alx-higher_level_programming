dc481c2212389021cd8eff380e672cff5b7dd442
kayode <kgbenga56@gmail.com>
2024-01-17 03:10:42 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -49,12 +49,13 @@ class Rectangle(Base):
         def y(self, value):
             self.__y = value
 
-            if __name__ =="__main__":
-                r1 = Rectangle(10, 2)
-                print(r1.id)
+    if __name__ == "__main__":
 
-                r2 = Rectangle(2, 10)
-                print(r2.id)
+    r1 = Rectangle(10, 2)
+    print(r1.id)
 
-                r3 = Rectangle(10, 2, 0, 0, 12)
-                print(r3.id)
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
