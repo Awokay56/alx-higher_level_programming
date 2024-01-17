b4996902203209f218a7222c64aa6c4e4bb5a701
kayode <kgbenga56@gmail.com>
2024-01-16 23:25:32 -0800

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,19 +18,19 @@ class Base:
             self.id = Base.__nb_objects
 
     if __name__ == "__main__":
-        b1 = Base( )
+        b1 = Base()
         print(b1.id)
 
-        b2 = Base( )
+        b2 = Base()
         print(b2.id)
 
-        b3 = Base( )
+        b3 = Base()
         print(b3.id)
 
         b4 = Base(12)
         print(b4.id)
 
-        b5 = Base( )
+        b5 = Base()
         print(b5.id)
 
 
