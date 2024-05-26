5fde3b2d0790d4b3f3c5bc8864e9c977be34f7ff
Angelkay <kgbenga56@gmail.com>
2024-05-26 07:59:01 -0700

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,3 @@
 -- Convert the entire database hbtn_0c_0 to UTF8.
 ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
